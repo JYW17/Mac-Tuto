d31087db513691e66cfeedb01884471d5e823614
JYW17 <cutr1005@naver.com>
2022-10-26 13:08:03 +0900

    2022-10-26 13:00

--- Python/test.py
+++ Python/test.py
@@ -9,9 +9,8 @@
 # for i in range(l):
 #     rst += (ord(x[i])-96)*(31**i)
 # print(rst)
-lis = [1,2,3,4,5]
-rst = 0
-for i in range(len(lis)):
-    rst += sum(lis[0:i+1])
-    print(rst)
-print(rst)
+dic = {'eye':1, 'face':2, 'shoes': 3}
+lis = []
+for i in dic:
+    lis.append(dic[i])
+print(lis)
